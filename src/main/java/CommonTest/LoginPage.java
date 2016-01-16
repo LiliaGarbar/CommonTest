1c80a0111da66cba36e2c594325acabdc6d65fdc
LiliaGarbar <garbar.lilia@gmail.com>
2016-01-17 00:51:19 +0200

    1 step

---
+++ src/main/java/CommonTest/LoginPage.java
@@ -0,0 +1,38 @@
+package CommonTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage
+{
+    private WebDriver driver;
+
+    private final By fieldLogin = By.name("login");
+    private final By fieldPass = By.name("password");
+    private final By submitButton = By.xpath("//span[text()='Р’РѕР№С‚Рё']");
+
+    public LoginPage enterLogin(String login){
+       driver.findElement(fieldLogin).sendKeys(login);
+        return this;
+    }
+
+    public LoginPage enterPassword(String password){
+        driver.findElement(fieldPass).sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickSubmit(){
+        driver.findElement(submitButton).click();
+        return this;
+    }
+
+    public LoginPage openLoginPage(){
+        driver.get("https://www.4invest.net/ru/login.html");
+        return this;
+    }
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+}
---
+++ src/test/java/CommonTest/LoginPageTest.java
@@ -0,0 +1,19 @@
+package CommonTest;
+
+
+
+@return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
